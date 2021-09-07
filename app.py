9b0c558d4d7b9d9c7d3608a60f3e49ea12b2ea6c
ZachWath <89270314+ZachWath@users.noreply.github.com>
2021-09-07 14:20:06 -0500

    final for submission
    cleaned things up by stream-lining functions , also wrapped the executable code in dunder main, and raised exceptions to prevent crashes.

--- app.py
+++ app.py
@@ -35,104 +35,134 @@ def balance_teams (team_mates):
     return ([Panthers,Bandits,Warriors])
         
 def display_stats(team):
-    player_list =''
-    total_players = len(team)
-    exp_players = 0
-    non_exp_players = 0
-    hieghts = 0
-    average_height = ''
-    guardians_list = ''
-    for person in team:
-        player_list = player_list + str(person ['name']) + ", "
-        if person ['experience'] == False:
-            non_exp_players += 1 
-        elif person ['experience'] == True:
-            exp_players += 1
-        hieghts += int(person ['height'])
-        guardians_list = guardians_list + str(person ['guardians']) + ', '
-    average_height = hieghts / total_players
+    try:
+        player_list =''
+        total_players = len(team)
+        exp_players = 0
+        non_exp_players = 0
+        hieghts = 0
+        average_height = ''
+        guardians_list = ''
+        for person in team:
+            player_list = player_list + str(person ['name']) + ", "
+            if person ['experience'] == False:
+                non_exp_players += 1 
+            elif person ['experience'] == True:
+                exp_players += 1
+            hieghts += int(person ['height'])
+            guardians_list = guardians_list + str(person ['guardians']) + ', '
+        average_height = hieghts / total_players
 
-    print ("*****************************")
-    print ("""
-        Total Players: {}\n
-        Total Expierenced Players: {}\n
-        Total Un-Expierenced Players: {}\n
-        Average Height: {}\n
-        """ .format(total_players, exp_players, non_exp_players, average_height))
-    print (""" 
-        Players on Team:
-            {}\n
-        Guardians:
-            {}
-        """.format(player_list, guardians_list))
-
+        print ("*****************************")
+        print ("""
+            Total Players: {}\n
+            Total Expierenced Players: {}\n
+            Total Un-Expierenced Players: {}\n
+            Average Height: {}\n
+            """ .format(total_players, exp_players, non_exp_players, average_height))
+        print (""" 
+            Players on Team:
+                {}\n
+            Guardians:
+                {}
+            """.format(player_list, guardians_list))
+    except TypeError:
+        team_selection()
 
 def select_option ():
-    selection = input("   Enter an option:   ")
-    while True:
-        if selection == "A":
+    
+        print("""\n\n BASKETBALL TEAM STATS TOOL!!!\n\n
+        ****MENU****\n\n
+        Here are your choices:\n
+        A) Display Team Stats\n
+        B) Quit\n
+        \n
+        """)
+        selection = input("   Enter an option:   ")
+        
+        if selection.upper() == "A":
             print("""
-                Which team would you like to view?\n
-                A) Panthers\n
-                B) Bandits\n
-                C) Warriors\n\n
-                """)
-            team_selection = input("  Enter an opption:   ")
-            if team_selection == "A":
+            Which team would you like to view?\n
+            A) Panthers\n
+            B) Bandits\n
+            C) Warriors\n\n
+            """)
+        elif selection.upper() == "B":
+            exit()
+        else:
+            print('please choose from the provided options...')
+            select_option()
+            
+
+def team_selection():
+
+    while True:
+        team_selection = input("  Enter an opption:   ")
+        if team_selection.upper() == "A":
                 print("\nTeam: Panthers...")
-                display_stats(Panthers)
+                return(Panthers)
                 
 
 
-            elif team_selection == "B":
+        elif team_selection.upper() == "B":
                 print("\nTeam: Bandits...")
-                display_stats (Bandits)
+                return(Bandits)
 
-            elif team_selection == "C":
+        elif team_selection.upper() == "C":
                 print("\nTeam: Warriors...")
-                display_stats(Warriors)
+                return(Warriors)  
+        else:
+            print ('please chooose from the provided options...')
+            continue
+
+
+def continue_question():
+    while True:
+        continue_selection = input ("Would you like to view another teams stats? (Y/N):    ")    
+        if continue_selection.upper() == 'Y':
+            break 
+        elif continue_selection.upper() == 'N':
+            quit_question = input ("Would you like to exit the program? (Y/N)?:    ")
+            if quit_question.upper() == 'Y':
+                exit() 
+            elif quit_question.upper()== 'N':
+                break
+            else:
+                print("please choose from the provided options....")  
+                continue              
+        else:
+            print("please choose from the provided options....")  
+            continue              
+
         
 
-        elif selection == "B":
-            exit()
-        continue_selection = input ("Would you like to view another teams stats? (Y/N):    ")
-        if continue_selection == 'Y':
-            print("""\n\n BASKETBALL TEAM STATS TOOL!!!\n\n
-            ****MENU****\n\n
-            Here are your choices:\n
-            A) Display Team Stats\n
-            B) Quit\n
-             \n
-             """)
-            break 
-        elif continue_selection == 'N':
-            exit()
+        
 
+                
+                
+        
             
     
             
 
 
-print("""\n\n BASKETBALL TEAM STATS TOOL!!!\n\n
-   ****MENU****\n\n
-   Here are your choices:\n
-   A) Display Team Stats\n
-   B) Quit\n
-   \n
-   """)
 
 
 
 
 
-clean_data()
+if __name__ == "__main__":
+    clean_data()
 
-teams_list2 = balance_teams (player_list)
-Panthers = teams_list2[0]
-Bandits = teams_list2[1]
-Warriors = teams_list2[2]
+    teams_list2 = balance_teams (player_list)
+    Panthers = teams_list2[0]
+    Bandits = teams_list2[1]
+    Warriors = teams_list2[2]
 
 
 
-while True:
-    select_option()
+    while True:
+        select_option()
+        selected_team = team_selection()
+        display_stats(selected_team)
+        continue_question()
